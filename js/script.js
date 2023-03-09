a49cde18ec6056c24ce1787825d80efa76f51057
MLibralato91 <michelelibralato911@gmail.com>
2023-03-09 15:50:16 +0100

    add my function

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ let myKm = document.querySelector(".km");
 const myPrice = 0.21;
 const juniorPrice = 0.20;
 const seniorPrice = 0.40;
-console.log(myAge);
+let totalPrice;
+
+document.getElementsByClassName("calcTicket").addEventListener("click", myFunctionCalc );
+
+function myFunctionCalc(){
+    totalPrice= myKm * myPrice;
+    
+}
+console.log(totalPrice);
+
