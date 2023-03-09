2bdbf6f456b0322cee0895a2eb58ca5d2f08a82d
MLibralato91 <michelelibralato911@gmail.com>
2023-03-09 18:17:42 +0100

    add bg

--- js/script.js
+++ js/script.js
@@ -27,13 +27,16 @@
 
 let myAge = document.querySelector("input.age");
 let myKm = document.querySelector("input.km");
+let myName = document.querySelector("input.myName");
 
 const myPrice = 0.21;
 const juniorPrice = 0.20;
 const seniorPrice = 0.40;
+
 let totalPrice;
 
-const ticket = document.querySelector('ticket');
+const ticket = document.querySelector('#ticket');
+const home = document.querySelector('#home');
 
 document.querySelector("button.calcTicket").addEventListener("click", myFunctionCalc );
 
@@ -49,14 +52,23 @@ function myFunctionCalc(){
     else
         totalPrice = myKm.value * myPrice;
     console.log(totalPrice);
-
+    let passenger = myName.value;
+    let kilometers = myKm.value;
     ticket.classList.remove('d-none');
+    home.classList.add('d-none');
+    document.getElementById('yourPrice').innerHTML += ` <p> ${totalPrice} €</p>`;
+    document.getElementById('passenger').innerHTML += ` <p> ${passenger} </p>`;
+    document.getElementById('kilometers').innerHTML += ` <p> ${kilometers} Km</p>`;
+
 }
 
 
 document.querySelector("button.resetTicket").addEventListener("click", myFunctionReset );
-
+document.querySelector("#ticket.button.resetTicket").addEventListener("click", myFunctionReset );
 function myFunctionReset(){
     myKm.value = '';
     myAge.value= '';
+    myName.value= '';
+    ticket.classList.add('d-none');
+    home.classList.remove('d-none');
 }
