3d5c5292c37af0bedf7d76ccff3a0ff67eeb8403
MLibralato91 <michelelibralato911@gmail.com>
2023-03-09 16:49:47 +0100

    add btn reset

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 
 
 
+// let myAge = document.querySelector("input.age");
+// let myKm = document.querySelector("input.km");
+
+// const myPrice = 0.21;
+// const juniorPrice = 0.20;
+// const seniorPrice = 0.40;
+// let totalPrice;
+
+
+
+// document.querySelector("button.calcTicket").addEventListener("click", myFunctionCalc );
+
+// function myFunctionCalc(){
+//     if(myAge.value < 18){
+//         totalPrice = myKm.value * myPrice - (myKm.value * myPrice * juniorPrice);
+//     }
+//     if(myAge.value >= 65){
+//         totalPrice = myKm.value * myPrice - (myKm.value * myPrice * seniorPrice);
+//     }
+//     console.log(totalPrice);
+// }
+
+// Milestone 1 Terminato
+
 let myAge = document.querySelector("input.age");
 let myKm = document.querySelector("input.km");
 
@@ -17,10 +41,20 @@ function myFunctionCalc(){
     if(myAge.value < 18){
         totalPrice = myKm.value * myPrice - (myKm.value * myPrice * juniorPrice);
     }
+    else
+        totalPrice = myKm.value * myPrice;
     if(myAge.value >= 65){
         totalPrice = myKm.value * myPrice - (myKm.value * myPrice * seniorPrice);
     }
+    else
+        totalPrice = myKm.value * myPrice;
     console.log(totalPrice);
 }
 
 
+document.querySelector("button.resetTicket").addEventListener("click", myFunctionReset );
+
+function myFunctionReset(){
+    myKm.value = '';
+    myAge.value= '';
+}
