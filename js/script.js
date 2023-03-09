dbdea145faa6d306aaba46bf398a6fa539fb5fa7
MLibralato91 <michelelibralato911@gmail.com>
2023-03-09 15:15:58 +0100

    add variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 
 
 
-const myAge = document.querySelector(".age");
-const myKm = document.querySelector(".km");
+let myAge = document.querySelector(".age");
+let myKm = document.querySelector(".km");
 
+const myPrice = 0.21;
+const juniorPrice = 0.20;
+const seniorPrice = 0.40;
 console.log(myAge);
